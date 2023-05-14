5e361088a213cdb64552b27ee7217caf6281b9e6
SujiYoun <msg0933@naver.com>
2023-05-15 00:42:36 +0900

    Create 21fin_Q4_1and2.c

---
+++ src/2021_FT/21fin_Q4_1and2.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS //8ok
+#include <stdio.h>
+#include <stdlib.h>
+
+void pick(int item[], int n, int* picked, int m, int toPick, int* sum_num)
+{
+	int i, j, smallest, lastIndex;
+	int sum;
+
+	if (toPick == 0) {
+		sum = 0;
+		for (i = 0; i < m; i++) {
+			//printf("%d", picked[i]);
+			sum += item[picked[i]];
+		}
+
+		if (sum == m) { //뽑은 1, 2의 합이 num과 같으면
+			/*for (i = 0; i < m; i++)
+				printf("%d", item[picked[i]]);
+			printf("\n");*/
+
+			//* 붙여야 sum_num 값에 접근 가능
+			*sum_num = *sum_num + 1;
+		}
+	}
+
+	//중복 조합
+	lastIndex = m - toPick - 1;
+	if (toPick == m)
+		smallest = 0;
+	else
+		smallest = picked[lastIndex];
+
+	if (toPick > 0) {
+		for (i = smallest; i < n; i++) {
+			picked[lastIndex + 1] = i;
+			//여기서 sum_num에 들어있는 건 주소값
+			pick(item, n, picked, m, toPick - 1, sum_num);
+		}
+	}
+}
+int main(void)
+{
+	int item[] = { 1, 2, 0 };
+	int* picked;
+	int num, sum_num = 0;
+
+	scanf("%d", &num);
+	picked = (int*)malloc(sizeof(int) * num);
+	if (picked == NULL)
+		return 0;
+
+	//주소값 넘겨주기
+	pick(item, 3, picked, num, num, &sum_num);
+	printf("%d\n", sum_num);
+
+	free(picked);
+	return 0;
+}
