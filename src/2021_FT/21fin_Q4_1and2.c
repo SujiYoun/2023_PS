17d6cb3c134671acb8bcc631cf387f571531fab8
SujiYoun <msg0933@naver.com>
2023-05-16 22:59:17 +0900

    Update 21fin_Q4_1and2.c

--- src/2021_FT/21fin_Q4_1and2.c
+++ src/2021_FT/21fin_Q4_1and2.c
@@ -14,7 +14,7 @@ void pick(int item[], int n, int* picked, int m, int toPick, int* sum_num)
 			sum += item[picked[i]];
 		}
 
-		if (sum == m) { //뽑은 1, 2의 합이 num과 같으면
+		if (sum == m && sum != 0) { //뽑은 1, 2의 합이 num과 같으면 picked안에 0뿐이면 0출력
 			/*for (i = 0; i < m; i++)
 				printf("%d", item[picked[i]]);
 			printf("\n");*/
@@ -34,6 +34,7 @@ void pick(int item[], int n, int* picked, int m, int toPick, int* sum_num)
 	if (toPick > 0) {
 		for (i = smallest; i < n; i++) {
 			picked[lastIndex + 1] = i;
+
 			//여기서 sum_num에 들어있는 건 주소값
 			pick(item, n, picked, m, toPick - 1, sum_num);
 		}
@@ -52,7 +53,7 @@ int main(void)
 
 	//주소값 넘겨주기
 	pick(item, 3, picked, num, num, &sum_num);
-	printf("%d\n", sum_num);
+	printf("%d", sum_num);
 
 	free(picked);
 	return 0;
