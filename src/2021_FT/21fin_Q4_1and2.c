7773dfb74e83039cc445b62e679e97047007d2f8
SujiYoun <msg0933@naver.com>
2023-05-19 20:39:36 +0900

    Update 21fin_Q4_1and2.c

--- src/2021_FT/21fin_Q4_1and2.c
+++ src/2021_FT/21fin_Q4_1and2.c
@@ -1,28 +1,25 @@
-#define _CRT_SECURE_NO_WARNINGS //8ok why?????????????????????
+#define _CRT_SECURE_NO_WARNINGS //9ok
 #include <stdio.h>
 #include <stdlib.h>
 
-void pick(int n, int* picked, int m, int toPick, int* sum_num)
+void pick(int items[], int n, int* picked, int m, int toPick, int tmpSum, int* sum_num)
 {
 	int i, j, smallest, lastIndex;
-	int sum;
 
 	if (toPick == 0) {
-		sum = 0;
+		/*sum = 0;
 		for (i = 0; i < m; i++) {
 			//printf("%d", picked[i]);
-			sum += picked[i];
-		}
+			sum += items[picked[i]];
+		}*/
 
-		if (sum == m) { //뽑은 1, 2의 합이 num과 같으면
-			/*for (i = 0; i < m; i++)
-				printf("%d", item[picked[i]]);
-			printf("\n");*/
+		tmpSum += items[picked[m - 1]]; //마지막으로 뽑은 수
 
-			//* 붙여야 sum_num 값에 접근 가능
-			*sum_num = *sum_num + 1;
+		if (tmpSum == m) { //뽑은 1, 2의 합이 num과 같으면
+			*sum_num = *sum_num + 1; //* 붙여야 sum_num 값에 접근 가능
 		}
 	}
+
 
 	//중복 조합
 	lastIndex = m - toPick - 1;
@@ -31,26 +28,28 @@ void pick(int n, int* picked, int m, int toPick, int* sum_num)
 	else
 		smallest = picked[lastIndex];
 
+	//**중간 계산 - 출력시간 줄이기
+	if (lastIndex > -1)
+		tmpSum += items[picked[lastIndex]];
+
+	if (tmpSum > m) //지금까지 뽑힌 수의 합이 m을 넘으면 재귀 호출 할 필요x.
+		return;
+
+
 	if (toPick > 0) {
-		for (i = smallest; i < n; i++) {
-			sum = 0;
-			for (j = 0; j <= lastIndex; j++)
-				sum += picked[j];
-
-			if (sum + i > m)
-				continue;
-
+		for (i = smallest; i < n; i++) { //반복문 안에서 중간계산 x
 			picked[lastIndex + 1] = i;
 
 			//여기서 sum_num에 들어있는 건 주소값
-			pick(n, picked, m, toPick - 1, sum_num);
+			pick(items, n, picked, m, toPick - 1, tmpSum, sum_num);
 		}
 	}
 }
 int main(void)
 {
+	int items[] = { 2, 1, 0 };
 	int* picked;
-	int num, sum_num = 0;
+	int num, sum_num = 0; //전역변수 사용x
 
 	scanf("%d", &num);
 	if (num < 1 || num > 900)
@@ -61,7 +60,7 @@ int main(void)
 		return 0;
 
 	//주소값 넘겨주기
-	pick(3, picked, num, num, &sum_num);
+	pick(items, 3, picked, num, num, 0, &sum_num);
 	printf("%d", sum_num);
 
 	free(picked);
